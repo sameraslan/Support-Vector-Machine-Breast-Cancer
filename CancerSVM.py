55f4fdd292bfb3bd82a68797bb9ab6e7e02055d7
sameraslan <82460915+sameraslan@users.noreply.github.com>
2021-07-15 19:57:34 -0400

    Added SVM Functionality

---
+++ CancerSVM.py
@@ -0,0 +1,24 @@
+#Predicting whether a tumor is malignant or benign using SVMs
+
+import sklearn
+from sklearn import datasets, svm, metrics
+from sklearn.neighbors import KNeighborsClassifier
+cancer = datasets.load_breast_cancer()
+# print(cancer.feature_names)
+# print(cancer.target_names)
+
+X = cancer.data
+y = cancer.target
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=.1)
+
+# print(x_train, " ", y_train)
+classes = ['malignant', 'benign']
+
+clf = svm.SVC(kernel="linear", C=1)
+clf.fit(x_train, y_train)
+
+yPred = clf.predict(x_test)
+acc = metrics.accuracy_score(y_test, yPred)
+
+print(acc)
