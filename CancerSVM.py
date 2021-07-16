69428fa1c84009434a22259da1415a8f3dc4f332
sameraslan <82460915+sameraslan@users.noreply.github.com>
2021-07-15 20:09:19 -0400

    Tested for best margin and kernel
    Used for loop to find average accuracy and manually tweaked margin and kernel

--- CancerSVM.py
+++ CancerSVM.py
@@ -9,16 +9,21 @@ cancer = datasets.load_breast_cancer()
 
 X = cancer.data
 y = cancer.target
+sumAcc = 0
+rangeNum = 10
+for i in range(rangeNum):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=.1)
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=.1)
+    # print(x_train, " ", y_train)
+    classes = ['malignant', 'benign']
 
-# print(x_train, " ", y_train)
-classes = ['malignant', 'benign']
+    clf = svm.SVC(kernel="linear", C=3)
+    clf.fit(x_train, y_train)
 
-clf = svm.SVC(kernel="linear", C=1)
-clf.fit(x_train, y_train)
+    yPred = clf.predict(x_test)
+    acc = metrics.accuracy_score(y_test, yPred)
+    print(acc)
+    sumAcc += acc
 
-yPred = clf.predict(x_test)
-acc = metrics.accuracy_score(y_test, yPred)
-
-print(acc)
+averageAcc = sumAcc / rangeNum
+print("Average Accuracy: ", averageAcc)
